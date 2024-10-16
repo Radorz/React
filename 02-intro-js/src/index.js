463d3fbff56377ff6468bd13e7f2a24b46ebc848
Luis Franco <luisin.francof@gmail.com>
2024-10-16 10:26:16 -0400

    Avances

---
+++ 02-intro-js/src/index.js
@@ -0,0 +1,14 @@
+console.log("Hola Mundo")
+
+// const and var 
+const nombre = 'El alfa';
+const apellido = 'El jefe'
+
+let valorDado = 'La crema';
+
+
+// No usar var 
+
+const complexString = `${nombre}  ${apellido} ${valorDado}`
+
+console.log(nombre, apellido, valorDado);
